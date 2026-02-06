249d44e3e4df1cb1f6ca94f4f1aaec408406988e
mando157 <mando.1572005@gmail.com>
2026-02-07 01:46:26 +0200

    animation featuers

--- js/script.js
+++ js/script.js
@@ -29,3 +29,22 @@ function typeing(){
 }
 typeing();
 
+
+const cards = document.querySelectorAll(".feature-card");
+
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      }
+      else{
+        entry.target.classList.remove("show");
+      }
+    });
+  }, {
+    threshold: 0.2
+  });
+
+  cards.forEach(card => observer.observe(card));
+
