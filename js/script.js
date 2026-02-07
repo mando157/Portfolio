2cf29526b8d18eb9bc895b62006f8b1b7bca2780
mando157 <mando.1572005@gmail.com>
2026-02-07 19:34:04 +0200

    links

--- js/script.js
+++ js/script.js
@@ -5,73 +5,72 @@ const typeing_text = document.getElementById("typing-text");
 let i = 0;
 let isDeleting = false;
 
-function typeing(){
-    if(!isDeleting){
-        typeing_text.innerText = text.substring(0 , i + 1);
-        i++;
+function typeing() {
+  if (!isDeleting) {
+    typeing_text.innerText = text.substring(0, i + 1);
+    i++;
 
-        if(i === text.length){
-            isDeleting = true;
-            setTimeout(typeing , 800);
-            return;
-        }
+    if (i === text.length) {
+      isDeleting = true;
+      setTimeout(typeing, 800);
+      return;
     }
-    else{
-        typeing_text.innerText = text.substring(0 , Math.max(i - 1 , 0));
-        i--;
-        if(i===0){
-            isDeleting = false;
-            setTimeout(typeing , 400);
-            return;
-        }
+  } else {
+    typeing_text.innerText = text.substring(0, Math.max(i - 1, 0));
+    i--;
+    if (i === 0) {
+      isDeleting = false;
+      setTimeout(typeing, 400);
+      return;
     }
-    setTimeout(typeing, isDeleting? 80 : 120);
+  }
+  setTimeout(typeing, isDeleting ? 80 : 120);
 }
 typeing();
 
-//* Features Card Animation 
+//* Features Card Animation
 
-const cards = document.querySelectorAll(".feature-card");
+const cards = document.querySelectorAll(".feature-card , .about-content");
 
-
-const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
       if (entry.isIntersecting) {
         entry.target.classList.add("show");
-      }
-      else{
+      } else {
         entry.target.classList.remove("show");
       }
     });
-  }, {
-    threshold: 0.2
-  });
+  },
+  {
+    threshold: 0.2,
+  },
+);
 
-  cards.forEach(card => observer.observe(card));
+cards.forEach((card) => observer.observe(card));
 
 //* Moving bar
 const words = document.querySelector(".moving");
 const container = document.querySelector(".motion-text");
 
-  let canMove = true;
-  let x = -words.offsetWidth;
+let canMove = true;
+let x = -words.offsetWidth;
 
-  container.addEventListener("mouseenter", () => {
-    canMove = false;
-  });
-  container.addEventListener("mouseleave", () => {
-    canMove = true;
-  });
+container.addEventListener("mouseenter", () => {
+  canMove = false;
+});
+container.addEventListener("mouseleave", () => {
+  canMove = true;
+});
 
-function move (){
-    if(canMove){
-        x += 2;
-        if(x > container.offsetWidth){
-            x = -words.offsetWidth;
-        }
-        words.style.transform = `translateX(${x}px)`
+function move() {
+  if (canMove) {
+    x += 2;
+    if (x > container.offsetWidth) {
+      x = -words.offsetWidth;
     }
-    requestAnimationFrame(move);
+    words.style.transform = `translateX(${x}px)`;
+  }
+  requestAnimationFrame(move);
 }
 move();
-
